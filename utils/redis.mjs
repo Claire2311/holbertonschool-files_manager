458ea5aef99f56195e58f1aadd53edeb519ac790
Claire2311 <claire.girard@ieseg.fr>
2025-01-10 13:01:05 +0100

    Task 0

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,8 +1,10 @@
 const redis = require('redis');
+const { promisify } = require('util');
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+    this.getAsync = promisify(this.client.get).bind(this.client);
     this.client.on('error', (err) => {
       console.log(`Error ${err}`);
     });
@@ -13,39 +15,17 @@ class RedisClient {
   }
 
   async get(key) {
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (err, reply) => {
-        if (err) {
-          console.error(`Error getting key ${key}:`, err);
-          return reject(err);
-        }
-        return resolve(reply);
-      });
-    });
+    const value = await this.getAsync(key);
+    return value;
   }
 
   async set(key, value, duration) {
-    return new Promise((resolve, reject) => {
-      this.client.set(key, value, 'EX', duration, (err) => {
-        if (err) {
-          console.error(`Error setting key ${key} with value ${value}:`, err);
-          return reject(err);
-        }
-        return resolve();
-      });
-    });
+    this.client.set(key, value);
+    this.client.expire(key, duration);
   }
 
   async del(key) {
-    return new Promise((resolve, reject) => {
-      this.client.del(key, (err) => {
-        if (err) {
-          console.error(`Error deleting key ${key}:`, err);
-          return reject(err);
-        }
-        return resolve();
-      });
-    });
+    this.client.del(key);
   }
 }
 
